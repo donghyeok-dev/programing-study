683e82c4d935c955d7ab8ba8c70e3e213c454d7c
donghyeok <donghyeok.jh@gmail.com>
2021-04-16 21:21:34 +0900

    Studying

--- src/study/chapter01/Memory.java
+++ src/study/chapter01/Memory.java
@@ -1,39 +1,35 @@
 package study.chapter01;
 
+import study.other.TestDto;
+
 public class Memory {
     static int a = 0;
 
     public static void main(String[] args) {
-        //-- Stack Segment main------------------->
-        String msg = "hello"; // Heap Segment에 저장됨.
+        String msg = "Hello"; // Heap Segment에 저장됨.
+        TestDto dto = new TestDto();
 
-        System.out.println("[Main] " +Integer.toHexString(System.identityHashCode(msg))); // 27f674d (참조주소)
+        dto.setName("My");
 
-        addMethod(msg);
-        /* 함수가 종료되면 msg의 참조주소가 바뀌는 것은
-           사실 처음부터 addMethod와 Main 메서드는 Stack 내에서 서로 다른 공간에 할당되어 있었기 때문입니다.
-           메서드 내에 할당된 참조값들은 heap에 생성되는데 이를 각각 참조하다가 addMethod가 종료되면서 스택포인터가 Main을 가르키고
-           Main의 msg가 보이게 되는 것입니다.
-            |       Stack        |                   Heap
-            ---------------------
-            |       Main        | --------------->  "hello"
-            |       addMethod   | --------------->  "hello world!"
+        addMethod(msg, dto);
 
+        /*
+        참조 객체로 전달된 dto만 메서드 내에서 변경한 값을 유지하고,
+        값 복사로 새롭게 생성된 변수는 메서드 내부에서 결과를 출력하고 함수 종료와 함께 GC 대상이 된다.
          */
-
-        System.out.println("[Main] " + Integer.toHexString(System.identityHashCode(msg))); // 27f674d (참조주소)
-
-        System.out.println(msg); // hello 2021
+        System.out.println("[Main] dto: " + dto.getName()); // My Love~
+        System.out.println("[Main] msg: " + msg); // Hello
     }
 
-    public static void addMethod(String msg) {
-        //-- Stack Segment addMethod------------------->
-        System.out.println("[addMethod] " + Integer.toHexString(System.identityHashCode(msg))); // 27f674d
+    /**
+     * @param msg : call-by-value : 새로운 객체를 생성한다.
+     * @param dto : call-by-reference : 기존 객체를 참조하고 있는 참조 객체를 생성한다.(포인터 개념)
+     */
+    public static void addMethod(String msg, TestDto dto) {
+        dto.setName(dto.getName() + " Love~");
+        msg += " world!";
 
-        msg += " world!";  // Heap Segment의 새로운 공간에 생성되고, addMethod의  msg는 새로운 공간을 가르킴.
-
-        System.out.println("[addMethod] " + Integer.toHexString(System.identityHashCode(msg))); // 1a407d53
-
-        System.out.println(msg); // hello world!
+        System.out.println("[addMethod] dto: " + dto.getName()); // My Love~
+        System.out.println("[addMethod] msg: " + msg); // Hello world!
     }
 }
---
+++ src/study/other/TestDto.java
@@ -0,0 +1,13 @@
+package study.other;
+
+public class TestDto {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
