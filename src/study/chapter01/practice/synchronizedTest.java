25c2b31ebdc5638b235b39f34756e67c72cd4875
donghyeok <donghyeok.jh@gmail.com>
2021-05-03 23:58:39 +0900

    study

--- src/study/chapter01/practice/synchronizedTest.java
+++ src/study/chapter01/practice/synchronizedTest.java
@@ -1,9 +1,25 @@
 package study.chapter01.practice;
 
 public class synchronizedTest {
+    private static boolean running = true;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        new Thread(() -> {
+           while(running) {
+               //System.out.println("running... " + (++count)); //멈춤
+               // 안멈춤
+           }
+        }).start();
+
+        System.out.println("Thread Start !");
+        Thread.sleep(1000);
+        running = false;
+        System.out.println("Thread Stop !");
+
+    }
+
+    public static void test1() {
         System.out.println("Test start!");
 
         new Thread(() -> {
@@ -25,9 +41,7 @@ public class synchronizedTest {
             }
         }).start();
         System.out.println("Test end!");
-
     }
-
     public static synchronized void callMe(String msg) throws InterruptedException {
         Thread.sleep(500);
         System.out.println("Call Me! : " + msg);
