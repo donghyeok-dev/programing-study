232fe6d65d539d62cc350d627f9f00eeae90c0f5
donghyeok <donghyeok.jh@gmail.com>
2021-05-04 23:31:20 +0900

    study

--- src/study/chapter01/practice/synchronizedTest.java
+++ src/study/chapter01/practice/synchronizedTest.java
@@ -7,7 +7,44 @@ public class synchronizedTest {
     private static AtomicBoolean running = new AtomicBoolean(true);
     private static AtomicInteger count = new AtomicInteger(0);
 
+    private static boolean s_running = true;
+    private static int s_count = 0;
+
+    public static synchronized boolean getRunning() {
+        return s_running;
+    }
+    public static synchronized void setRunning(boolean val) {
+        s_running = val;
+    }
+
+//    public static synchronized int getCount() {
+//        return s_count;
+//    }
+    public static synchronized int incrementCount(String user) {
+        s_count += 1;
+        System.out.println(">> incrementCount s_count: " + s_count + " > " + user);
+        return s_count;
+    }
+
     public static void main(String[] args) throws InterruptedException {
+
+        Thread s = new Thread(() -> {
+            while (getRunning()) {
+                System.out.println("S1 Running... " + incrementCount("S1"));
+            }
+        });
+
+        Thread s2 = new Thread(() -> {
+            while (getRunning()) {
+                System.out.println("S2 Running... " + incrementCount("S2"));
+            }
+        });
+
+        Thread s3 = new Thread(() -> {
+            while (getRunning()) {
+                System.out.println("S3 Running... " + incrementCount("S3"));
+            }
+        });
 
         Thread t = new Thread(() -> {
            while(running.get()) {
@@ -33,12 +70,20 @@ public class synchronizedTest {
             }
         });
 
-        t.start();
-        t2.start();
-        t3.start();
+        s.start();
+        s2.start();
+        s3.start();
+
+//        t.start();
+//        t2.start();
+//        t3.start();
+
         System.out.println("Thread Start !");
         Thread.sleep(100);
+
+        setRunning(false);
         running.set(false);
+
         System.out.println("Thread Stop !");
         System.out.println(">> count after: " + count.get());
     }
