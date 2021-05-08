3992fdfc295b31280f683df3c8adcd998f35dd8c
donghyeok <donghyeok.jh@gmail.com>
2021-05-08 23:56:15 +0900

    study

---
+++ src/study/chapter01/practice/InstrumentationAgent.java
@@ -0,0 +1,21 @@
+package study.chapter01.practice;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * https://www.baeldung.com/java-size-of-object
+ */
+public class InstrumentationAgent {
+    private static volatile Instrumentation globalInstrumentation;
+
+    public static void premain(final String agentArgs, final Instrumentation inst) {
+        globalInstrumentation = inst;
+    }
+
+    public static long getObjectSize(final Object object) {
+        if (globalInstrumentation == null) {
+            throw new IllegalStateException("Agent not initialized.");
+        }
+        return globalInstrumentation.getObjectSize(object);
+    }
+}
