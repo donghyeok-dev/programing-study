4209e37b4184eb97cba50287fbf78a0a5f5f6feb
donghyeok <donghyeok.jh@gmail.com>
2021-05-18 23:45:48 +0900

    study

--- src/study/chapter01/practice/SerializationTest.java
+++ src/study/chapter01/practice/SerializationTest.java
@@ -9,7 +9,7 @@ public class SerializationTest {
         final String FILE_PATH =  "C:\\Users\\mosic\\IdeaProjects\\programing-study\\src\\study\\chapter01\\practice\\test.serialization.cv";
         
         //직렬화
-        try (FileOutputStream   fos = new FileOutputStream (FILE_PATH);
+        /*try (FileOutputStream   fos = new FileOutputStream (FILE_PATH);
              ObjectOutputStream oos =new ObjectOutputStream (fos);
         ) {
             Apple apple = new Apple();
@@ -17,7 +17,7 @@ public class SerializationTest {
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
 
         // 역직렬화
         try(FileInputStream fis=new FileInputStream(FILE_PATH);
@@ -34,12 +34,25 @@ public class SerializationTest {
     /*
     implements Serializable를 안하면 런타임 예외 발생함.
     java.io.NotSerializableException: study.chapter01.practice.SerializationTest$Apple
+
+    직렬화 -> 직렬화 부분 주석 & Apple 클래스 필드 추가 -> 역직렬화하면 아래 처럼 오류
+    InvalidClassException: study.chapter01.practice.SerializationTest$Apple; local class incompatible: stream classdesc serialVersionUID = -88973244
+    
+    다시 직렬화 -> ... 위 과정 반복해보면
+    Apple에 직렬화 버전 정보를 추가하면 오류가 없어짐.
+
+    serialVersionUID는 직렬화&역직렬화 하는데 있어 식별ID로 사용되기 때문에
+    직렬화 -> 필드 변화 -> 역직렬화해도 정상 작동하게 된다.
      */
     @ToString
     static class Apple implements Serializable{
+        private static final long serialVersionUID = 1L;
+
         int amount = 1000;
         String location = "청송";
         static String cDate = "2021-05-17";
         transient String cDate2 = "2021-05-17";
+        int temp;
+        int temp2;
     }
 }
